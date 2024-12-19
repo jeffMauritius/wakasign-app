f077a4025b36ac59523c51a5e697986f49a528cd
jeff <jfroussel75@gmail.com>
2024-12-19 08:55:14 +0400

    create doker-compose file to run postgres

---
+++ lib/prisma.ts
@@ -0,0 +1,15 @@
+// Disable TypeScript to avoid troubles with `global.` and avoid vscode import troubles
+// @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+import { PrismaClient } from '@prisma/client';
+
+export const prisma: PrismaClient =
+  global.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
